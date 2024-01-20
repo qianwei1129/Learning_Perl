cb656ff876c028dc7d7c5009e8aff7aad75cde6b
Win <2990834217@qq.com>
2024-01-20 23:10:06 +0800

    update Chapter_4 and Question_4

--- Questions/Question_4.pl
+++ Questions/Question_4.pl
@@ -5,3 +5,11 @@ use utf8;
 binmode(STDOUT,":encoding(gbk)");
 binmode(STDIN,":encoding(gbk)");
 binmode(STDERR,":encoding(gbk)");
+
+sub max{
+	if ($_[0] > $_[1]){
+		$_[0];
+	}else {
+		$_[1];
+	}
+}
